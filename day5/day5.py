cbb566789678412428a2c027a4c567c1a0b329c0
JULIA REYNOLDS <julia.reynolds@kasasa.com>
2020-12-06 16:30:00 -0600

    day 5 solution

--- day5/day5.py
+++ day5/day5.py
@@ -1,80 +1,73 @@
-import re
+
+def findMaxSeatID(filename):
+    boarding_passes_binary, boarding_passes_decimal = findSeatIDs(filename)
+
+    boarding_passes_decimal.sort(reverse=True)
+    #print(boarding_passes_binary)
+    #print(boarding_passes_decimal)
+
+    return boarding_passes_decimal[0]
 
 
-def countValidPassports(filename):
-    valid_count = 0
-    passports = []
-
-    populatePassportsFromFile(filename, passports)
-    print("found passports: " + str(len(passports)))
-    for passport in passports:
-
-        if len(passport.keys()) == 8:
-            if validateFields(passport):
-                valid_count += 1
-        else:
-            if len(passport.keys()) == 7:
-                if "cid" not in passport.keys():
-                    if validateFields(passport):
-                        valid_count += 1
-    return valid_count
+def findSeatIDs(filename):
+    boarding_passes_binary = []
+    boarding_passes_decimal = []
+    populateBoardingPassesFromFile(filename, boarding_passes_binary)
+    for binary_pass in boarding_passes_binary:
+        seatid = binaryToDecimal(int(binary_pass))
+        boarding_passes_decimal.append(seatid)
+    return boarding_passes_binary, boarding_passes_decimal
 
 
-def validateFields(passport):
-    regexForBYR = r"(19[2-8][0-9]|199[0-9]|200[0-2])"
-    regexForIYR = r"(201[0-9]|2020)"
-    regexForEYR = r"(202[0-9]|2030)"
-    regexForCM = r"(1[5-8][0-9]|19[0-3])cm$"
-    regexForIN = r"(59|6[0-9]|7[0-6])in$"
-    regexForWebColor = r"^#(?:[0-9a-fA-F]{6}){1,2}$"
-    regexForEyeColor = r"^(amb|blu|brn|gry|grn|hzl|oth)$"
-    regexForPID = r"^\d\d\d\d\d\d\d\d\d$"
+def findEmptySeatID(filename):
+    boarding_passes_binary, boarding_passes_decimal = findSeatIDs(filename)
 
-    validFieldCount = 0
+    boarding_passes_decimal.sort()
 
-    validFields = False
+    index = 0
+    emptySeatID = 0
 
-    if re.match(regexForBYR, passport["byr"]) :
-        validFieldCount += 1
-    if re.match(regexForIYR, passport["iyr"]) :
-        validFieldCount += 1
-    if re.match(regexForEYR, passport["eyr"]) :
-        validFieldCount += 1
-    if re.match(regexForCM, passport["hgt"])  or re.match(regexForIN, passport["hgt"]) :
-        validFieldCount += 1
-    if re.match(regexForWebColor, passport["hcl"]) :
-        validFieldCount += 1
-    if re.match(regexForEyeColor, passport["ecl"]) :
-        validFieldCount += 1
-    if re.match(regexForPID, passport["pid"]) :
-        validFieldCount += 1
+    while index < len(boarding_passes_decimal) :
+        seatID = boarding_passes_decimal[index]
+        nextSeatID = boarding_passes_decimal[index+1]
 
-    if validFieldCount == 7:
-        validFields = True
+        if nextSeatID != seatID + 1 :
+            emptySeatID = seatID + 1
+            index = len(boarding_passes_decimal)
 
-    return validFields
+        index += 1
+
+    return(emptySeatID)
 
 
-def populatePassportsFromFile(filename, passports):
-    total_passports = 1
+def populateBoardingPassesFromFile(filename, boardingPasses):
     with open(filename, "r") as fp:
-
-        passport = {}
         for entry in fp:
             entry = entry.replace('\n', '')
+            entry = entry.replace('B', '1')
+            entry = entry.replace('F', '0')
+            entry = entry.replace('R', '1')
+            entry = entry.replace('L', '0')
 
-            if len(entry) > 1:
-                pairs = entry.split(' ')
-                for pair in pairs:
-                    passport[pair.split(':')[0]] = pair.split(':')[1]
-            else:
-                total_passports += 1
-                passports.append(passport)
-                passport = {}
-    passports.append(passport)
-    print("total in file:" + str(total_passports))
+            boardingPasses.append(entry)
 
+    # Function calculates the decimal equivalent
+    # to given binary number
+
+
+def binaryToDecimal(binary):
+    binary1 = binary
+    decimal, i, n = 0, 0, 0
+    while (binary != 0):
+        dec = binary % 10
+        decimal = decimal + dec * pow(2, i)
+        binary = binary // 10
+        i += 1
+    #print(decimal)
+    return decimal
 
 if __name__ == "__main__":
-    result1 = countValidPassports("input.txt")
+    result1 = findMaxSeatID("input.txt")
+    result2 = findEmptySeatID("input.txt")
     print(result1)
+    print(result2)
--- day5/day5_test.py
+++ day5/day5_test.py
@@ -3,7 +3,7 @@ import day5
 
 class Day5TestCase(unittest.TestCase):
     def test_day5(self):
-        self.assertEqual(day5.countValidPassports("test_input.txt"), 2)
+        self.assertEqual(day5.findMaxSeatID("test_input.txt"), 820)
 
 if __name__ == '__main__':
     unittest.main()
